1ef452b5bfec0fb5dee09dc533770f5faea3ccfa
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-03-10 12:21:26 +0600

    :bulb: generate QR code

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('connected');
+
+// =========== Generate QR CODE =======//
+// URL=https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=dhaka
+
+// ============= get QR Image ID ==========//
+let QR = document.getElementById('QR-img');
+
+// ========== Generate QR Code Function =========//
+const generateCode=async(qr)=>{
+    const url=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qr}`;
+    QR.src=url;
+};
+
+// ============= Get Input Value ==========//
+const getInputValue =()=>{
+    const inputValue = document.getElementById('field');
+    const input = inputValue.value;
+    console.log(inputValue.value);
+    inputValue.value ='';
+    generateCode(input);
+}
