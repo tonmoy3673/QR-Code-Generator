a2a047846a3bc7a907972c033fd9e8f6fa39eb8e
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-03-10 13:49:56 +0600

    :tada: completed project and validate input field

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ console.log('connected');
 
 // ============= get QR Image ID ==========//
 let QR = document.getElementById('QR-img');
+// =========== Get button =========//
+let button = document.getElementById('BTN');
+//  ============ Input Field ==========//
+let input = document.getElementById('field');
 
 // ========== Generate QR Code Function =========//
 const generateCode=async(qr)=>{
     const url=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qr}`;
+    QR.style.display='block'
     QR.src=url;
+    button.disabled=true;
 };
 
-// ============= Get Input Value ==========//
-const getInputValue =()=>{
-    const inputValue = document.getElementById('field');
-    const input = inputValue.value;
-    console.log(inputValue.value);
-    inputValue.value ='';
-    generateCode(input);
-}
+// ============= Check Input Value ==========//
+input.addEventListener('input',function(){
+   button.disabled = input.value.trim() ==='';
+   if (input.value) {
+    button.disabled =false;
+   }
+})
+
+// ============ Set input Value ===========//
+button.addEventListener('click',function (){
+    let inputField= input.value;
+    generateCode(inputField)
+    input.value = '';
+    
+})
